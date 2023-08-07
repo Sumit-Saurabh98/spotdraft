8b8f17c4aeac6ac0c2fe230ff67cecc3d6585352
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2023-08-07 18:27:30 +0530

    d

--- index.js
+++ index.js
@@ -1,46 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Airtable = require('airtable');
-require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
-
 app.use(bodyParser.json());
 
-const base = new Airtable({ apiKey: process.env.API_KEY }).base(process.env.BASE);
-
 app.post('/asana-webhook', (req, res) => {
-  const eventData = req.body.events[0];
+    const eventData = req.body.events[0];
+    if (eventData.action === 'added') {
+        const taskData = eventData.resource;
 
-  if (eventData.action === 'added') {
-    const taskData = eventData.resource;
-
-    base('Asana Tasks').create(
-      [
-        {
-          fields: {
-            'Task ID': taskData.gid,
-            Name: taskData.name,
-            Assignee: taskData.assignee ? taskData.assignee.name : null,
-            'Due Date': taskData.due_on,
-            Description: taskData.notes,
-          },
-        },
-      ],
-      (err, record) => {
-        if (err) {
-          console.error(err);
-          res.sendStatus(500);
-        } else {
-          console.log('Record added to Airtable:', record.getId());
-          res.sendStatus(200);
-        }
-      }
-    );
-  } else {
+        const base = new Airtable({accessToken: process.env.API_KEY}).base(process.env.BASE);
+        base('Asana Tasks').create([
+            {
+                "fields": {
+                    "Task ID": taskData.gid,
+                    "Name": taskData.name,
+                    "Assignee": taskData.assignee ? taskData.assignee.name : null,
+                    "Due Date": taskData.due_on,
+                    "Description": taskData.notes
+                }
+            }
+        ], (err, record) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log('Record added to Airtable:', record.getId());
+        });
+    }
     res.sendStatus(200);
-  }
 });
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(3000, () => console.log('Server is running on port 3000'));
